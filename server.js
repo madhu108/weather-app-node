b25c2b26c8fd2fa75b4dc6412a9ed7cf03e2b39f
tuka <tukaramdas.kks@gmail.com>
2017-10-18 22:02:19 +0200

    final upload with responsive UI

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3500;
 var app = express();
 
 app.use(express.static(__dirname + '/public'));
 
 app.listen(port, (req, res) => {
-	console.log('server is on port 3000');
+	console.log('server is on port 3500');
 });
