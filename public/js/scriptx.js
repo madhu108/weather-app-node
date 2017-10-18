a8fdd3a64f91ce371b845ae2e9ba063df8aa7092
tuka <tukaramdas.kks@gmail.com>
2017-10-18 13:28:01 +0200

    new scriptx.js & search location

---
+++ public/js/scriptx.js
@@ -0,0 +1,58 @@
+$(document).ready(function () {
+	var inputCity;
+	var lat;
+	var lon;
+	var cC;
+	var owmUrl = "http://api.openweathermap.org/data/2.5/weather?units=imperial&q=";
+	var apiKey = "&appid=6d066bc084f1eb21dcd8d852fb0d02e1";
+	var weatherFun = function () {
+
+		$.getJSON(owmUrl + inputCity + apiKey, function (data) {
+			var temp = data.main.temp;
+			var celsia = ((temp - 32) / 1.8).toFixed(1);
+			var farenheit = (data.main.temp).toFixed(1);
+			var wind = data.wind.speed;
+			var icon = data.weather[0].icon;
+			var desc = data.weather[0].description;
+
+			$('#icondiv').empty().append('<img src="https://openweathermap.org/img/w/' + icon + '.png" height="52" width="52">');
+			$('#descriptiondiv').empty().append('<p>' + desc + '</p>');
+			$(" #loc").text(inputCity);
+			$(" #temp-p").text(celsia + " °C");
+			$(" #wind").text(wind + " m/s");
+			$(".toggle").on("click", (function () {
+				$(".f").toggleClass("selected"),
+					$(".c").toggleClass("unselected");
+				if ($(".f").hasClass("selected")) {
+					$(" #temp-p").text(farenheit + " °F");
+				} else {
+					$(" #temp-p").text(celsia + " °C");
+				}
+			}));
+		});
+
+	};
+
+	$("#city").on('keyup', function (cityName) {
+		if (cityName.keyCode == 13) {
+			inputCity = $('#city').val();
+			weatherFun(inputCity);
+		}
+	});
+
+	$('#go').click(function () {
+		inputCity = $('#city').val();
+		weatherFun(inputCity);
+	});
+
+	$.get("https://ipinfo.io", function (response) {
+		console.log(response.ip, response.city);
+		inputCity = response.city;
+		weatherFun(inputCity);
+	}, "jsonp");
+
+	//	navigator.geolocation.getCurrentPosition(function (position) {
+	//				console.log(position.coords.latitude, position.coords.longitude);
+	//});
+
+});
