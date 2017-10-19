619761bbb0ba6cbe9f6cdef88cd6b126cc2d3cb8
tuka <tukaramdas.kks@gmail.com>
2017-10-19 19:45:01 +0200

    if - geo,else - ipapi

--- public/js/scriptx.js
+++ public/js/scriptx.js
@@ -1,90 +1,99 @@
 $(document).ready(function () {
 
-	var owmUrl = "https://api.openweathermap.org/data/2.5/weather?units=imperial&q=";
-	var owmUrlll = 'https://apiopenweathermap.org/data/2.5/weather?units=imperial&lat=48.5581776&lon=18.1764059&appid=6d066bc084f1eb21dcd8d852fb0d02e1'
+//	var owmUrl = "https://api.openweathermap.org/data/2.5/weather?units=imperial&q=";
+//	var owmUrlll = 'https://apiopenweathermap.org/data/2.5/weather?units=imperial&lat=48.5581776&lon=18.1764059&appid=6d066bc084f1eb21dcd8d852fb0d02e1'
 	var inputCity;
-	var apiKey = "&appid=6d066bc084f1eb21dcd8d852fb0d02e1";
 	var lat;
 	var lon;
-	var weatherFun = function () {
+	var apiKey = "6d066bc084f1eb21dcd8d852fb0d02e1";
 
-		$.getJSON(owmUrl + inputCity + apiKey, function (data) {
-			console.log(data);
-			var temp = data.main.temp;
-			var celsia = ((temp - 32) / 1.8).toFixed(1);
-			var farenheit = (data.main.temp).toFixed(1);
-			var wind = data.wind.speed;
-			var icon = data.weather[0].icon;
-			var desc = data.weather[0].description;
+	var cityFun = function () {
 
-			$('#icondiv').empty().append('<img src="https://openweathermap.org/img/w/' + icon + '.png" height="52" width="52">');
-			$('#descriptiondiv').empty().append('<p>' + desc + '</p>');
-			$(" #loc").text(data.name);
-			$(" #temp-p").text(celsia + " °C");
-			$(" #wind").text(wind + " m/s");
-			$(".toggle").on("click", (function () {
-				$(".f").toggleClass("selected"),
-					$(".c").toggleClass("unselected");
-				if ($(".f").hasClass("selected")) {
-					$(" #temp-p").text(farenheit + " °F");
-				} else {
-					$(" #temp-p").text(celsia + " °C");
-				}
-			}));
-		});
+		$.getJSON(`https://api.openweathermap.org/data/2.5/weather?units=imperial&q=${inputCity}&appid=${apiKey}`, function (data) {
+		console.log(data);
+		var temp = data.main.temp;
+		var celsia = ((temp - 32) / 1.8).toFixed(1);
+		var farenheit = (data.main.temp).toFixed(1);
+		var wind = data.wind.speed;
+		var icon = data.weather[0].icon;
+		var desc = data.weather[0].description;
+
+		$('#icondiv').empty().append('<img src="https://openweathermap.org/img/w/' + icon + '.png" height="52" width="52">');
+		$('#descriptiondiv').empty().append('<p>' + desc + '</p>');
+		$(" #loc").text(data.name);
+		$(" #temp-p").text(celsia + " °C");
+		$(" #wind").text(wind + " m/s");
+		$(".toggle").on("click", (function () {
+			$(".f").toggleClass("selected"),
+				$(".c").toggleClass("unselected");
+			if ($(".f").hasClass("selected")) {
+				$(" #temp-p").text(farenheit + " °F");
+			} else {
+				$(" #temp-p").text(celsia + " °C");
+			}
+		}))
+	});
 
 	};
+	var latLonFun = function(){
+		$.getJSON(`https://apiopenweathermap.org/data/2.5/weather?units=imperial&lat=${lat}&lon=${lon}$&appid={apiKey}`, function (data) {
+		console.log(data);
+		var temp = data.main.temp;
+		var celsia = ((temp - 32) / 1.8).toFixed(1);
+		var farenheit = (data.main.temp).toFixed(1);
+		var wind = data.wind.speed;
+		var icon = data.weather[0].icon;
+		var desc = data.weather[0].description;
 
+		$('#icondiv').empty().append('<img src="https://openweathermap.org/img/w/' + icon + '.png" height="52" width="52">');
+		$('#descriptiondiv').empty().append('<p>' + desc + '</p>');
+		$(" #loc").text(data.name);
+		$(" #temp-p").text(celsia + " °C");
+		$(" #wind").text(wind + " m/s");
+		$(".toggle").on("click", (function () {
+			$(".f").toggleClass("selected"),
+				$(".c").toggleClass("unselected");
+			if ($(".f").hasClass("selected")) {
+				$(" #temp-p").text(farenheit + " °F");
+			} else {
+				$(" #temp-p").text(celsia + " °C");
+			}
+		}))
+	});
+	};
+	var err = function(){
+		console.log(`geoloc error`);
+		$.get("https://ipinfo.io", function (response) {
+			inputCity = response.city;
+			console.log(`inputCity: ${inputCity}`);
+			cityFun();
+		}, "jsonp");
+	}
+	
 
 	if (navigator.geolocation) {
 		navigator.geolocation.getCurrentPosition(function (position) {
 			lat = position.coords.latitude;
 			lon = position.coords.longitude;
 			console.log(`lat: ${lat} and lon: ${lon}`);
-			$.getJSON(`https://apiopenweathermap.org/data/2.5/weather?units=imperial&lat=${lat}&lon=${lon}${apiKey}`, function (data) {
-				console.log(data);
-				var temp = data.main.temp;
-				var celsia = ((temp - 32) / 1.8).toFixed(1);
-				var farenheit = (data.main.temp).toFixed(1);
-				var wind = data.wind.speed;
-				var icon = data.weather[0].icon;
-				var desc = data.weather[0].description;
-
-				$('#icondiv').empty().append('<img src="https://openweathermap.org/img/w/' + icon + '.png" height="52" width="52">');
-				$('#descriptiondiv').empty().append('<p>' + desc + '</p>');
-				$(" #loc").text(data.name);
-				$(" #temp-p").text(celsia + " °C");
-				$(" #wind").text(wind + " m/s");
-				$(".toggle").on("click", (function () {
-					$(".f").toggleClass("selected"),
-						$(".c").toggleClass("unselected");
-					if ($(".f").hasClass("selected")) {
-						$(" #temp-p").text(farenheit + " °F");
-					} else {
-						$(" #temp-p").text(celsia + " °C");
-					}
-				}));
-			});
-		});
-	} else {
-		$.get("https://ipinfo.io", function (response) {
-			inputCity = response.city;
-			weatherFun(inputCity);
-		}, "jsonp");
-	}
+			latLonFun();			
+		},err());
+	};
 	
+	
+
 	$("#city").on('keyup', function (cityName) {
 		if (cityName.keyCode == 13) {
 			inputCity = $('#city').val();
-			weatherFun(inputCity);
+			cityFun();
 		}
 	});
 	$('#go').click(function () {
 		inputCity = $('#city').val();
-		weatherFun(inputCity);
+		weatherFun();
 	});
 
-	
+
 
 
 
